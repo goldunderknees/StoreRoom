93377a65ae51bc76438a43d9c837c1b683da7d61
Kin Lui <kinlui@Kins-MacBook-Pro.local>
2015-06-21 11:45:38 +0800

    Initial Commit

---
+++ StoreRoom/StoreRoom.h
@@ -0,0 +1,19 @@
+//
+//  StoreRoom.h
+//  StoreRoom
+//
+//  Created by Kin Lui on 21/6/15.
+//  Copyright (c) 2015 goldunderknees. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for StoreRoom.
+FOUNDATION_EXPORT double StoreRoomVersionNumber;
+
+//! Project version string for StoreRoom.
+FOUNDATION_EXPORT const unsigned char StoreRoomVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <StoreRoom/PublicHeader.h>
+
+
